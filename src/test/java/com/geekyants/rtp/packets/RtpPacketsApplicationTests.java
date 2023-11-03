8b07527cb0aceefdbbe2c12c60ffa6fbb83a7aea
Arpit Kumar <arpitkumar4000@gmail.com>
2023-11-03 12:01:09 +0530

    Initial commit

---
+++ src/test/java/com/geekyants/rtp/packets/RtpPacketsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.geekyants.rtp.packets;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RtpPacketsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
