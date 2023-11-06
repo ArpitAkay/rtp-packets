0f6dc943563b68122991d02050223656cb0964d4
Arpit Kumar <arpitkumar4000@gmail.com>
2023-11-06 12:49:18 +0530

    Refactor code

--- src/main/java/com/geekyants/rtp/packets/RtpPacketsCapture.java
+++ src/main/java/com/geekyants/rtp/packets/RtpPacketsCapture.java
@@ -65,7 +65,7 @@ public class RtpPacketsCapture {
 
         // Tell the handle to loop using the listener we created
         try {
-            int maxPackets = 500;
+            int maxPackets = 1500;
             handle.loop(maxPackets, listener);
         } catch (InterruptedException | PcapNativeException | NotOpenException e) {
             e.printStackTrace();
