328f5b5c38d21a27d79aa74af614a39da26c0e7f
Arpit Kumar <arpitkumar4000@gmail.com>
2023-11-07 19:56:12 +0530

    Refactored code

--- src/main/java/com/geekyants/rtp/packets/RtpPacketsApplication.java
+++ src/main/java/com/geekyants/rtp/packets/RtpPacketsApplication.java
@@ -2,18 +2,11 @@ package com.geekyants.rtp.packets;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class RtpPacketsApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(RtpPacketsApplication.class, args);
-	}
-
-	@Bean
-	public RestTemplate restTemplate() {
-		return new RestTemplate();
 	}
 }
